4046a591731cb8e9e7858b5bdc4cf22ee5b61626
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-13 01:38:44 +0300

    about sayfasına giden yol oluşturuldu

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MyNavbar from "../components/Navbar/MyNavbar";
 import Home from "../pages/home/Home";
 import Footer from "../components/Footer/Footer";
+import About from "../pages/about/About";
 
 const AppRouter = () => {
   return (
@@ -11,7 +12,9 @@ const AppRouter = () => {
         <MyNavbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
+
         <Footer />
       </BrowserRouter>
     </div>
