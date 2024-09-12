75e09d36ed8699fcd7fdd2fd6840930281a2d869
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-13 01:47:31 +0300

    home sayfası güncellendi

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <Container className="text-center text-light home-container">
       <p>Hi! My Name is</p>
-      <h1 className="text-danger border-bottom p-2">Nurdoğan Bahadır</h1>
+      <h1 className="text-danger border-bottom p-2 w-100">Nurdoğan Bahadır</h1>
       <p>Frontend Web Developer</p>
     </Container>
   );
